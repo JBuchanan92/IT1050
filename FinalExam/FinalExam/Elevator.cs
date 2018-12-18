63c41672e98cfd1ad104f62c011e249296a98be1
JBuchanan92 <43155185+JBuchanan92@users.noreply.github.com>
2018-12-17 23:56:43 -0500

    Add files via upload

---
+++ FinalExam/FinalExam/Elevator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExam
+{
+    class Elevator
+    {
+
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            this.MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            this.Occupants[index] = passenger;
+        }
+
+        public double GetCurrentWeight()
+        {
+            double totalWeight = 0;
+
+            foreach (Passenger passenger in Occupants)
+            {
+                totalWeight += passenger.GetWeight();
+            }
+
+            return totalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            if (GetCurrentWeight() > MaxWeight)
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
