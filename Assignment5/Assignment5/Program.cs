1c514bdf370dff7b6a8b4580e77c6e4ae0045b62
JBuchanan92 <43155185+JBuchanan92@users.noreply.github.com>
2018-12-18 23:58:14 -0500

    Add files via upload

---
+++ Assignment5/Assignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GameBoard.CreateBoard();
+            GameBoard.PrintBoard();
+            GameBoard.UpdateBoard();
+            GameBoard.AskToContinue();
+
+            System.Threading.Thread.Sleep(50000);
+        }
+    }
+    
+}
---
+++ Assignment5/Assignment5/Question.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Question
+    {
+
+        public static string AskForString(string question)
+        {
+            System.Console.WriteLine(question);
+            string answer = System.Console.ReadLine();
+
+            return answer;
+        }
+
+        public static int AskForInt(string question)
+        {
+            int answer = int.Parse(AskForString(question));
+
+            if (answer >= GameBoard.BOARDSIZE)
+            {
+                AskForInt("Please enter a number between 0 to 7");
+            }
+
+            return answer;
+        }
+    }
+}
